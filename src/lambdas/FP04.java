dece4de5f6c7289328d44090e376d735a5540932
Justin Lemmons <justinlemmons7@gmail.com>
2022-05-21 00:26:09 -0400

    Worked on some Comparator objects and how to sort objects in a variety of formats using getter methods and lamda Comparators on a stream

--- src/lambdas/FP04.java
+++ src/lambdas/FP04.java
@@ -1,6 +1,8 @@
 package lambdas;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Course{
 	private String name; 
@@ -56,12 +58,37 @@ public class FP04 {
 				new Course("Microservices", "Microservices", 98, 20000), 
 				new Course("FullStack", "FullStack", 98, 20000), 
 				new Course("AWS", "Cloud", 100, 20000), 
-				new Course("Azure", "Cloud", 75, 25000), 
-				new Course("Azure", "Cloud", 91, 20000), 
+				new Course("Azure", "Cloud", 75, 25000),
 				new Course("Docker", "Cloud", 81, 20000), 
 				new Course("Kubernetes", "Cloud", 87, 25000)
-				
-				); 
+				);
+		System.out.println(courses.stream().allMatch(course -> course.getReviewScore() >= 75));
+		System.out.println(courses.stream().noneMatch(course -> course.getReviewScore() <= 75)); 
+		
+		List<Course> courseNumOfStudents = courses.stream().filter(course -> course.getNumOfStudents() > 20000)
+				.collect(Collectors.toList()); 
+		System.out.println(courseNumOfStudents); 
+		
+		Comparator<Course> comparingByNoOfStudentsIncreasing = Comparator.comparingInt(Course::getNumOfStudents); 
+		System.out.println(courses.stream().sorted(comparingByNoOfStudentsIncreasing).collect(Collectors.toList())); 
+		
+		Comparator<Course> comparingByNoOfStudentsDecreasing = Comparator.comparingInt(Course::getNumOfStudents).reversed(); 
+		System.out.println(courses.stream().sorted(comparingByNoOfStudentsDecreasing).collect(Collectors.toList())); 
+		
+		Comparator<Course> comparingByNumOfStudentsAndNumOfReviews = Comparator.comparingInt(Course::getNumOfStudents)
+				.thenComparing(Course::getReviewScore).reversed(); 
+		System.out.println(courses.stream().sorted(comparingByNumOfStudentsAndNumOfReviews).collect(Collectors.toList()));
+		
+		Comparator<Course> namesInOrder = Comparator.comparing(Course::getName); 
+		System.out.println(courses.stream().sorted(namesInOrder).collect(Collectors.toList())); 
+	
+		
+		
+		
+
+
+
+
 	}
 	
 
